cc4271c3a4b8e7a1aea2949f18d7c024698cb420
thameemsh <thameemsh@gmail.com>
2022-09-02 23:44:51 -0400

    Add chart component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 import "./Expenses.css";
 const Expenses = (props) => {
@@ -29,6 +30,7 @@ const Expenses = (props) => {
               date={item.date}
             />
           ))} */}
+          <ExpensesChart expenses = {filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
